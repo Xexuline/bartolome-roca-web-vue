85e5ab270dbfd718d1ba88c046b762a02fac2791
mrcodedev <mrcodedev@gmail.com>
2019-02-11 16:20:37 +0100

    Adding component Text

--- src/pages/Web.vue
+++ src/pages/Web.vue
@@ -3,17 +3,17 @@
     <app-navigation />
     <div class="container-web">
       <app-section title="Inicio" image="wall" />
-      <app-text></app-text>
+      <app-text title="Bartolomé Roca título h2"></app-text>
       <app-footer></app-footer>
     </div>
   </div>
 </template>
 
 <script>
-  import Navbar from "../components/Navbar.vue";
-  import Section from "../components/Section.vue";
-  import Text from "../components/Text.vue";
-  import Footer from "../components/Footer.vue";
+  import Navbar from "@/components/Navbar.vue";
+  import Section from "@/components/Section.vue";
+  import Text from "@/components/Text.vue";
+  import Footer from "@/components/Footer.vue";
 
   export default {
     name: "Web",
