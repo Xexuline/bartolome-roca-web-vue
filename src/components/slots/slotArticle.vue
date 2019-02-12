e5f24fea50d43cce58b8bddc6b42fa8490647ab4
xexuline <jscentella@gmail.com>
2019-02-12 18:52:18 +0100

    create basic slot template

---
+++ src/components/slots/slotArticle.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <div class="article-container">
+      <h2 class="h2-article-title">{{ title }}</h2>
+      <slot></slot>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "app-article-slot",
+  props: {
+    title: String
+  },
+}
+</script>
+<style>
+@import "../../assets/css/components/text.css";
+</style>
+
+
