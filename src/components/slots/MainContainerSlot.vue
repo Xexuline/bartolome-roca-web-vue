d6ddd1241bee7855e7a81dc5d99a1b44b11256e6
xexuline <jscentella@gmail.com>
2019-02-13 19:16:15 +0100

    created container slot

---
+++ src/components/slots/MainContainerSlot.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container-root">
+    <app-navigation />
+    <div class="container-web">
+      <app-section :route="checkRoute()" />
+      <slot></slot>
+      <app-footer />
+    </div>
+  </div>
+</template>
+<script>
+import Navbar from "@/components/Navbar.vue";
+import Section from "@/components/Section.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    "app-navigation": Navbar,
+    "app-section": Section,
+    "app-footer": Footer
+  },
+  methods: {
+    checkRoute() {
+      return this.$route.name;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
